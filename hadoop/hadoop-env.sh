348bfcba152d19df88218ada8d849a15e371c67d
liguirong98@163.com <hadoop@vm1.(none)>
2016-05-06 20:54:29 +0800

    snn

--- hadoop/hadoop-env.sh
+++ hadoop/hadoop-env.sh
@@ -22,7 +22,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME=${JAVA_HOME}
+export JAVA_HOME=/usr/local/java/jdk1.7.0_80/
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
